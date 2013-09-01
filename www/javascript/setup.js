38af9a4266b8ff6d7b9e75d0b1c18f503d751646
breaker1 <breaker1@gmail.com>
2013-09-01 09:00:30 -0400

    Removed an absolute path from the setup javascript file

--- www/javascript/setup.js
+++ www/javascript/setup.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
     
     $("#driver").change(function() {
-        $.get("/setup.php/config/" + $("#driver").val() ,"", function(data){
+        $.get("setup.php/config/" + $("#driver").val() ,"", function(data){
            $("#driverConfig").html(data);
         }, "html");
         
